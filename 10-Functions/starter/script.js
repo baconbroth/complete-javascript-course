0a1e97e1fac2cecea4303461dc12a0f1de30278a
baconbroth <jason.chen94@gmail.com>
2022-05-22 14:47:19 -0400

    test

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,39 @@
 'use strict';
+
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section!
+  answers: new Array(4).fill(0),
+  registerNewAnswer: function () {
+    //get answer
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+      )
+    );
+    // Register Answer
+    typeof answer === 'number' &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
+  },
+};
+//poll
+const pollRegisterNewAnswer = poll.registerNewAnswer.bind(poll);
+document
+  .querySelector('.poll')
+  .addEventListener('click', pollRegisterNewAnswer);
+
+//bonus
+// [5, 2, 3];
+// [1, 5, 3, 9, 6, 1];
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
